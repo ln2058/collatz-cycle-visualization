691e77d84a9b42615aa7fd459f5a7ccc240ff6be
ln2058 <88479783+ln2058@users.noreply.github.com>
2021-08-08 15:46:39 +0900

    Update collatz_master.py
    A lot more user-friendly and accepts negative inputs

--- collatz_master.py
+++ collatz_master.py
@@ -1,51 +1,88 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-max_num = 10000
-result = np.zeros((max_num, 3))
 
-for n in range(1, max_num+1):
-    # State rules for odd and even
+def input_function():
+    # Enter values with appropriate operations in terms of n
+    rule_even = input("Enter rule for even integer: ")
+    rule_odd = input("Enter rule for odd integer: ")
+    min_num = int(input("Enter minimum start value: "))
+    max_num = int(input("Enter maximum start value: "))
+    return rule_even, rule_odd, min_num, max_num
 
-    # Have variables to store data
-    tracker = [n]
-    i = n
-    while True:
-        if (n % 2) == 0:
-            n = int((3*n)-1)
-            if n in tracker:
-                result[i-1, 0] = tracker[0]
-                result[i-1, 1] = len(tracker)
-                tracker.sort()
-                result[i-1, 2] = tracker[-1]
-                break
-            else:
-                tracker.append(n)
-        else:
-            n = int((n/2)+0.5)
-            if n in tracker:
-                result[i-1, 0] = tracker[0]
-                result[i-1, 1] = len(tracker)
-                tracker.sort()
-                result[i-1, 2] = tracker[-1]
-                break
-            else:
-                tracker.append(n)
 
-# print(result)
+def collatz_master():
+    rule_even, rule_odd, min_num, max_num = input_function()
+    result = np.zeros((abs(max_num)+abs(min_num)+1, 3))
+    i = -1
+    for n in range(min_num, max_num+1):
+        # State rules for odd and even
 
-num = np.array(result[:, 0])
-iterations = np.array(result[:, 1])
-max_num = np.array(result[:, 2])
+        # Have variables to store data
+        tracker = [n]
+        i += 1
+        while True:
+            if (n % 2) == 0:    # even
+                n = int(eval(rule_even))
+                if n in tracker:
+                    result[i, 0] = tracker[0]   # counts as initial number
+                    # counts as number of iterations
+                    result[i, 1] = len(tracker)
+                    tracker.sort()
+                    min = tracker[0]
+                    max = tracker[-1]
+                    if abs(min) > abs(max):  # check to see if max is negative number of positive
+                        result[i, 2] = min
+                    elif abs(min) < abs(max):
+                        result[i, 2] = max
+                    elif abs(min) == abs(max):
+                        result[i, 2] = max
+                    break
+                else:
+                    tracker.append(n)
+            else:     # off
+                n = int(eval(rule_odd))
+                if n in tracker:
+                    result[i, 0] = tracker[0]
+                    result[i, 1] = len(tracker)
+                    tracker.sort()
+                    min = tracker[0]
+                    max = tracker[-1]
+                    if abs(min) > abs(max):  # check to see if max is negative number of positive
+                        result[i, 2] = min
+                    elif abs(min) < abs(max):
+                        result[i, 2] = max
+                    elif abs(min) == abs(max):
+                        result[i, 2] = max
+                    break
+                else:
+                    tracker.append(n)
 
-f = plt.figure(1)
-plt.title("Number of iterations")
-plt.plot(num, iterations, 'ro', ms=2, color='blue')
-f.show()
+    # print(result) # if you want in array form
 
-g = plt.figure(2)
-plt.title("Max value reached in cycle")
-plt.plot(num, max_num, 'ro', ms=2, color='blue')
-g.show()
+    # Plotting data on graphs
+    num = np.array(result[:, 0])
+    iterations = np.array(result[:, 1])
+    max_num = np.array(result[:, 2])
 
-plt.show()
+
+    f = plt.figure(1)
+    plt.title("Number of iterations")
+    plt.plot(num, iterations, 'ro', ms=0.5, color='blue')
+    f.show()
+
+    g = plt.figure(2)
+    plt.title("Max value reached in cycle")
+    plt.plot(num, max_num, 'ro', ms=0.5, color='blue')
+
+    max_num.sort()
+    min_bound = float((max_num[0])/300)
+    max_bound = float((max_num[-1])/300)
+
+    plt.ylim(min_bound, max_bound)  # scaling the y axis
+    g.show()
+
+    plt.show()
+
+
+collatz_master()
